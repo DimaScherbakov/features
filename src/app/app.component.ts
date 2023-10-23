9d78eb88d5cd1603161344aa7de5ba9b3d95781c
dmytro <dmytro.shcherbakov@utechcorp.com>
2023-10-23 15:45:52 +0300

    add timeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Options } from '@angular-slider/ngx-slider';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tooltip';
+  sliderValue = 0;
+  intervals = [
+    {
+      "from": 1698006775,
+      "to": 1698007378
+    },
+    {
+      "from": 1698011218,
+      "to": 1698014000
+    },
+    {
+      "from": 1698014989,
+      "to": 1698020676
+    },
+    {
+      "from": 1698020887,
+      "to": 1698030000
+    }
+  ];
+
+  sliderMin = 0;
+  sliderMax = 100;
+
+  value: number = 0;
+  highValue: number = 100;
+  options: Options = {
+    floor: 0,
+    ceil: 100,
+    showTicks: true,
+    step: 1,
+    hideLimitLabels: false,
+    hidePointerLabels: false,
+    showSelectionBar: false
+  };
+
+  normalizeTimestamp(timestamp: number): number {
+    let minTimestamp = this.intervals[0].from;
+    let maxTimestamp = this.intervals[this.intervals.length - 1].to;
+
+    return ((timestamp - minTimestamp) / (maxTimestamp - minTimestamp)) * (this.sliderMax - this.sliderMin);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NgxSliderModule } from 'ngx-slider-v2';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
